9ac95649090686013977157c877e9dcf6533df24
Romain Salvi <salvir.main@gmail.com>
2025-04-29 19:15:10 +0200

    Update train.py

--- train.py
+++ train.py
@@ -9,8 +9,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--n-episodes', default=100000, type=int)
     parser.add_argument('--print-every', default=2000, type=int)
-    parser.add_argument('--device', default='cpu', type=str)
-    parser.add_argument('--device', default='cpu', type=str)
+    parser.add_argument('--device', default='cuda', type=str)
     return parser.parse_args()
 
 args = parse_args()
