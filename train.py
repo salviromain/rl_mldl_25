c428ba0829fa511ffc1e98a3674db065dfd86a53
Romain Salvi <salvir.main@gmail.com>
2025-04-29 16:02:38 +0200

    Update train.py

--- train.py
+++ train.py
@@ -12,8 +12,8 @@ from agent import Agent, Policy
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n-episodes', default=40000, type=int, help='Number of training episodes')
-    parser.add_argument('--print-every', default=5000, type=int, help='Print info every <> episodes')
+    parser.add_argument('--n-episodes', default=100000, type=int, help='Number of training episodes')
+    parser.add_argument('--print-every', default=10000, type=int, help='Print info every <> episodes')
     parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
 
     return parser.parse_args()
