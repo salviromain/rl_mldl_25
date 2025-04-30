7d16c6542c2147cbd2107ff63a699a6dc3f3ef5c
MattiaSernia294330 <mattiasernia.ms@gmail.com>
2025-04-30 11:54:06 +0200

    new pseudocode

---
+++ agent1.py
@@ -0,0 +1,175 @@
+import numpy as np
+import torch
+import wandb
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+def discount_rewards(r, gamma):
+    discounted_r = torch.zeros_like(r)
+    running_add = 0
+    for t in reversed(range(0, r.size(-1))):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
+
+class Critic(torch.nn.Module):
+    def _init_(self, state_space):
+        super()._init_()
+        self.state_space = state_space
+        """
+            Critic network
+        """
+        # TASK 3: critic network for actor-critic algorithm
+        self.model=torch.nn.Sequential(
+            torch.nn.Linear(self.state_space, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 1),
+        )
+
+
+        self.model.apply(self.init_weights)
+
+    def init_weights(self,m):
+        if isinstance(m, torch.nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                torch.init.zeros_(m.bias)
+    def forward(self, state):
+        value=self.model(state)
+        return value
+
+class Policy(torch.nn.Module):
+    def _init_(self, state_space, action_space):
+        super()._init_()
+        self.state_space = state_space
+        self.action_space = action_space
+        self.hidden = 64
+        self.tanh = torch.nn.Tanh()
+
+        """
+            Actor network
+        """
+        self.fc1_actor = torch.nn.Linear(state_space, self.hidden)
+        self.fc2_actor = torch.nn.Linear(self.hidden, self.hidden)
+        self.fc3_actor_mean = torch.nn.Linear(self.hidden, action_space)
+        
+        # Learned standard deviation for exploration at training time 
+        self.sigma_activation = F.softplus
+        init_sigma = 0.5
+        self.sigma = torch.nn.Parameter(torch.zeros(self.action_space)+init_sigma)
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear:
+                torch.nn.init.normal_(m.weight)
+                torch.nn.init.zeros_(m.bias)
+
+
+    def forward(self, x):
+        """
+            Actor
+        """
+        x_actor = self.tanh(self.fc1_actor(x))
+        x_actor = self.tanh(self.fc2_actor(x_actor))
+        action_mean = self.fc3_actor_mean(x_actor)
+
+        sigma = self.sigma_activation(self.sigma)
+        normal_dist = Normal(action_mean, sigma)
+        return normal_dist
+
+class Agent(object):
+    def _init_(self, policy,critic, device='cpu'):
+        self.train_device = device
+        self.policy = policy.to(self.train_device)
+        self.critic = critic.to(self.train_device)
+        self.optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
+        self.critic_optimizer = torch.optim.Adam(critic.parameters(), lr=1e-3)
+        self.gamma = 0.99
+        self.states = []
+        self.next_states = []
+        self.action_log_probs = []
+        self.rewards = []
+        self.done = []
+
+
+    def update_policy(self,I):
+        action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)
+
+        states = torch.stack(self.states, dim=0).to(self.train_device).squeeze(-1)
+
+        next_states = torch.stack(self.next_states, dim=0).to(self.train_device).squeeze(-1)
+
+        rewards = torch.stack(self.rewards, dim=0).to(self.train_device).squeeze(-1)
+
+        done = torch.Tensor(self.done).to(self.train_device)
+
+        self.states, self.next_states, self.action_log_probs, self.rewards, self.done = [], [], [], [], []
+
+        #
+        # TASK 2:
+        #   - compute discounted returns
+        #   - compute policy gradient loss function given actions and returns
+        #   - compute gradients and step the optimizer
+        #
+
+
+        #
+        # TASK 3:
+        v_next = self.get_critic(next_states)
+        v_next = (1 - done) * v_next  # zero-out terminal states
+        v=self.get_critic(state)
+        delta=rewards+self.gamma*v_next-v
+        policy_loss=I*delta*action_log_probs
+        self.optimizer.zero_grad()
+        policy_loss.backward()
+        self.optimizer.step()
+        I = gamma*I
+        wandb.log({"policy_loss":policy_loss.item()})
+        #wandb.log({"Q_value":Q_value.item()})
+
+        return delta, v, I
+
+    def update_critic(self, delta, v, state):
+        critic_loss=delta*v
+        self.optimizer.zero_grad()
+        critic_loss.backward()
+        wandb.log({"critic_loss":critic_loss.item()})
+        self.critic_optimizer.step()
+
+
+        
+
+
+    def get_action(self, state, evaluation=False):
+        """ state -> action (3-d), action_log_densities """
+        x = torch.from_numpy(state).float().to(self.train_device)
+
+        normal_dist = self.policy(x)
+
+        if evaluation:  # Return mean
+            return normal_dist.mean, None
+
+        else:   # Sample from the distribution
+            action = normal_dist.sample()
+
+            # Compute Log probability of the action [ log(p(a[0] AND a[1] AND a[2])) = log(p(a[0])*p(a[1])*p(a[2])) = log(p(a[0])) + log(p(a[1])) + log(p(a[2])) ]
+            action_log_prob = normal_dist.log_prob(action).sum()
+
+            return action, action_log_prob
+    def get_critic(self, state):
+        x = state.float().to(self.train_device)
+
+        value= self.critic(x)
+        return value
+
+    def store_outcome(self, state, next_state, action_log_prob, reward, done):
+        self.states.append(torch.from_numpy(state).float())
+        self.next_states.append(torch.from_numpy(next_state).float())
+        self.action_log_probs.append(action_log_prob)
+        self.rewards.append(torch.Tensor([reward]))
+        self.done.append(done)
---
+++ train1.py
@@ -0,0 +1,98 @@
+"""Train an RL agent on the OpenAI Gym Hopper environment using
+    REINFORCE and Actor-critic algorithms
+"""
+import argparse
+
+import torch
+import gym
+import wandb
+
+from env.custom_hopper import *
+from agent1 import Agent, Policy, Critic
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n-episodes', default=100000, type=int, help='Number of training episodes')
+    parser.add_argument('--print-every', default=5000, type=int, help='Print info every <> episodes')
+    parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
+
+    return parser.parse_args()
+
+args = parse_args()
+
+wandb.init(project="hopper-rl", config={
+    "episodes": args.n_episodes,
+    "print_every": args.print_every,
+    "device": args.device,
+    "lr_policy": 1e-3,
+    "lr_critic": 1e-3,
+    "gamma": 0.99
+})
+
+def main():
+
+	env = gym.make('CustomHopper-source-v0')
+	# env = gym.make('CustomHopper-target-v0')
+
+	print('Action space:', env.action_space)
+	print('State space:', env.observation_space)
+	print('Dynamics parameters:', env.get_parameters())
+
+
+	"""
+		Training
+	"""
+	observation_space_dim = env.observation_space.shape[-1]
+	action_space_dim = env.action_space.shape[-1]
+
+	policy = Policy(observation_space_dim, action_space_dim)
+	critic = Critic(observation_space_dim)
+	agent = Agent(policy, critic, device=args.device)
+
+    #
+    # TASK 2 and 3: interleave data collection to policy updates
+    #
+
+	for episode in range(args.n_episodes):
+		done = False
+
+		train_reward = 0
+		
+		state = env.reset()  # Reset the environment and observe the initial state
+		
+		I=1
+		
+		while not done:  # Loop until the episode is over
+		
+			action, action_log_prob=agent.get_action(state)
+		
+			previous_state = state
+		
+			state, reward, done, info = env.step(action.detach().cpu().numpy())
+
+			agent.store_outcome(previous_state, state, action_log_prob, reward, done)
+
+			train_reward += reward
+
+			delta, v , I= agent.update_policy(I)
+
+			agent.update_critic(delta, v , previous_state)
+
+
+		wandb.log({"episode":episode,"return":train_reward})
+		
+		if (episode+1)%args.print_every == 0:
+
+			print('Training episode:', episode)
+
+			print('Episode return:', train_reward)
+
+
+	torch.save(agent.policy.state_dict(), "model_new_pseudocode.mdl")
+	wandb.save("model_critic_modified")
+	wandb.finish()
+	
+
+if _name_ == '_main_':
+	main()
