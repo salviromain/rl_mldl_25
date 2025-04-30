304fd34d5ad138b51bc72c9c1c7b431041a2b1f2
Romain Salvi <salvir.main@gmail.com>
2025-04-30 09:00:42 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default=None, type=str, help='Model path')
     parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
-    parser.add_argument('--render', default=False, action='store_true', help='Render the simulator')
+    parser.add_argument('--render', default=True, action='store_true', help='Render the simulator')
     parser.add_argument('--episodes', default=10, type=int, help='Number of test episodes')
 
     return parser.parse_args()
