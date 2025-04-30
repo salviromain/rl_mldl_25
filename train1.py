600aabe0bdb968c7410ff9631955245a640c627f
MattiaSernia294330 <mattiasernia.ms@gmail.com>
2025-04-30 14:07:46 +0200

    forse ho trovato il problema2

--- train1.py
+++ train1.py
@@ -77,7 +77,7 @@ def main():
 
 			delta, v , I= agent.update_policy(I)
 
-			agent.update_critic(delta , previous_state)
+			agent.update_critic(delta ,  previous_state, v)
 
 
 		wandb.log({"episode":episode,"return":train_reward})
