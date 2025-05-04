9906705dc299244156435a0c0b69b45e5f275a08
Romain Salvi <salvir.main@gmail.com>
2025-05-05 01:37:55 +0200

    Update train.py

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ import wandb
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n-episodes', default=10000, type=int)
+    parser.add_argument('--n-episodes', default=100000, type=int)
     parser.add_argument('--batch-episodes', default=10, type=int)
-    parser.add_argument('--print-every', default=1000, type=int)
+    parser.add_argument('--print-every', default=2000, type=int)
     parser.add_argument('--device', default='cpu', type=str)
     return parser.parse_args()
 
