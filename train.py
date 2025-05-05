665f626f67c43480a2f756df3d30f67eae30cc33
Romain Salvi <salvir.main@gmail.com>
2025-05-06 00:06:25 +0200

    Update train.py

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ def main():
         #},
         {
            "label": "BaselineMean",
-           "agent": Agent(Policy(obs_dim, act_dim), lr=1e-3, entropy_coeff=0.0, device=args.device),
+           "agent": Agent(Policy(obs_dim, act_dim), lr=1e-4, entropy_coeff=0.01, device=args.device),
            "use_baseline": True,
            "baseline_val": 0.0
         },
