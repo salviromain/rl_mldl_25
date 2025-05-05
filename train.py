f922fd248bc2c301b5ff9a190ad2e71215080ae4
Romain Salvi <salvir.main@gmail.com>
2025-05-05 16:47:54 +0200

    50k runs

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import wandb
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n-episodes', default=100000, type=int)
+    parser.add_argument('--n-episodes', default=50000, type=int)
     parser.add_argument('--batch-episodes', default=10, type=int)
     parser.add_argument('--print-every', default=2000, type=int)
     parser.add_argument('--device', default='cpu', type=str)
