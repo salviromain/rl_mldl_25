6672b9dd119393d9247188bad01547ec4db405f4
Romain Salvi <salvir.main@gmail.com>
2025-06-17 22:59:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default=None, type=str, help='Model path')
     parser.add_argument('--device', default='cpu', type=str, help='Network device [cpu, cuda]')
-    parser.add_argument('--render', default=True, action='store_true', help='Render the simulator')
+    parser.add_argument('--render', default=False, action='store_true', help='Render the simulator')
     parser.add_argument('--episodes', default=10, type=int, help='Number of test episodes')
     parser.add_argument('--save_path', default='test_rewards_plot.png', type=str, help='Path to save reward plot')
     return parser.parse_args()
@@ -55,7 +55,7 @@ def main():
     rewards = np.array(rewards)
     mean = rewards.mean()
     std = rewards.std()
-    np.save("REINFORCE.npy",rewards)
+    #np.save("REINFORCE.npy",rewards)
 
     plt.figure(figsize=(10, 5))
     plt.plot(episodes, rewards, label='Episode Return')
